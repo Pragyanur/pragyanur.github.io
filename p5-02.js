18b54dbb7106b74860acb0ada81c90900c89df08
Pragyanur <pragyanur.babu@gmail.com>
2023-07-11 04:06:49 +0530

    update

--- p5-02.js
+++ p5-02.js
@@ -1,9 +1,11 @@
+let canvas;
+
 function setup() {
-
-}
-
-function draw() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0, 0);
     canvas.style('z-index', '-1');
+}
+
+function draw() {
+    
 }
