baeefc5c247ad70f871f86961c21e49fa17ad5b1
Pragyanur <pragyanur.babu@gmail.com>
2023-07-12 20:18:17 +0530

    update

--- scripts/resumeBackground.js
+++ scripts/resumeBackground.js
@@ -3,9 +3,9 @@ let canvas;
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
-  canvas.style('z-index', '-1');
+  canvas.style('z-index', '3');
 }
 
 function draw() {
-  
+  circle(mouseX, mouseY, 100);
 }
