044f78b0bfa46421cf0ea02cf4e0cee376408137
Pragyanur <pragyanur.babu@gmail.com>
2023-07-15 19:23:57 +0530

    update

---
+++ scripts/sketch-03.js
@@ -0,0 +1,27 @@
+let L1, L2, L3, L4, L5
+
+
+function setup() {
+  L1 = createGraphics(windowWidth, windowHeight, document.getElementById("L1"));
+  L2 = createGraphics(windowWidth, windowHeight, document.getElementById("L2"));
+  L3 = createGraphics(windowWidth, windowHeight, document.getElementById("L3"));
+  L4 = createGraphics(windowWidth, windowHeight, document.getElementById("L4"));
+  L5 = createGraphics(windowWidth, windowHeight, document.getElementById("L5"));
+  
+
+}
+
+function draw() {
+
+}
+
+class wheel {
+  constructor(posX, posY) {
+    this.x = posX;
+    this.y = posY;
+
+  }
+  draw() {
+    
+  }
+}
