782ebdff9b728479debabc60891c54262f25b85c
Pragyanur <pragyanur.babu@gmail.com>
2023-07-16 15:08:33 +0530

    update

--- scripts/sketch-03.js
+++ scripts/sketch-03.js
@@ -1,27 +1,33 @@
-let L1, L2, L3, L4, L5
+let L1, L2, L3, L4, L5;
 
 
 function setup() {
-  L1 = createGraphics(windowWidth, windowHeight, document.getElementById("L1"));
-  L2 = createGraphics(windowWidth, windowHeight, document.getElementById("L2"));
-  L3 = createGraphics(windowWidth, windowHeight, document.getElementById("L3"));
-  L4 = createGraphics(windowWidth, windowHeight, document.getElementById("L4"));
-  L5 = createGraphics(windowWidth, windowHeight, document.getElementById("L5"));
-  
+  createCanvas(windowWidth, windowHeight);
+  L1 = createGraphics(windowWidth, windowHeight);
+  L2 = createGraphics(windowWidth, windowHeight);
+  L3 = createGraphics(windowWidth, windowHeight);
+  L4 = createGraphics(windowWidth, windowHeight);
+  L5 = createGraphics(windowWidth, windowHeight);
+  background(0);
+  L1.background(0, 50);               // 20% transparent black background
+  L2.background(0, 50);               // 20% transparent black background
+  L3.background(0, 50);               // 20% transparent black background
+  L4.background(0, 50);               // 20% transparent black background
+  L5.background(0, 50);               // 20% transparent black background
 
 }
 
 function draw() {
+  circle(100, 100, 40);
+  L1.circle(200, 100, 40);
+  L2.circle(300, 100, 40);
+  L3.circle(400, 100, 40);
+  L4.circle(500, 100, 40);
+  L5.circle(600, 100, 40);
+  image(L1, 0, 0);
+  image(L2, 0, 0);
+  image(L3, 0, 0);
+  image(L4, 0, 0);
+  image(L5, 0, 0);
 
-}
-
-class wheel {
-  constructor(posX, posY) {
-    this.x = posX;
-    this.y = posY;
-
-  }
-  draw() {
-    
-  }
 }
