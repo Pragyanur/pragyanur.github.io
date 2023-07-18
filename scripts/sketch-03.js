5d16f920e7afee6fbe9642465848ad5f0260fa73
Pragyanur <pragyanur.babu@gmail.com>
2023-07-18 13:05:38 +0530

    update

--- scripts/sketch-03.js
+++ scripts/sketch-03.js
@@ -1,33 +1,69 @@
-let L1, L2, L3, L4, L5;
+class wheelObject {
+  constructor(posX, posY, size) {
+    this.X = posX;
+    this.Y = posY;
+    this.diameter = size;
+    this.xSqueeze = this.diameter * 9 / 10;
+  }
+  show() {
+    stroke(255);
+    fill(0, 100);
+    ellipse(this.X + 50, this.Y, this.xSqueeze - 10, this.diameter - 10);
+    stroke(200);
+    fill(200, 30, 20);
+    rect(this.X, this.Y - this.diameter / 2, 30, this.diameter);
+    stroke(255);
+    fill(0, 100);
+    beginShape(POINTS);
+    vertex()
 
+    endShape();
+    ellipse(this.X, this.Y, this.xSqueeze, this.diameter);
+    ellipse(this.X, this.Y, this.xSqueeze - 30, this.diameter - 30);
+  }
+
+  animate() {
+
+  }
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  L1 = createGraphics(windowWidth, windowHeight);
-  L2 = createGraphics(windowWidth, windowHeight);
-  L3 = createGraphics(windowWidth, windowHeight);
-  L4 = createGraphics(windowWidth, windowHeight);
-  L5 = createGraphics(windowWidth, windowHeight);
-  background(0);
-  L1.background(0, 50);               // 20% transparent black background
-  L2.background(0, 50);               // 20% transparent black background
-  L3.background(0, 50);               // 20% transparent black background
-  L4.background(0, 50);               // 20% transparent black background
-  L5.background(0, 50);               // 20% transparent black background
 
+  // L1 = createGraphics(windowWidth, windowHeight);
+  // L2 = createGraphics(windowWidth, windowHeight);
+  // L3 = createGraphics(windowWidth, windowHeight);
+  // L4 = createGraphics(windowWidth, windowHeight);
+  // L5 = createGraphics(windowWidth, windowHeight);
+  // L1.background(0, 50);               // 20% transparent black background
+  // L2.background(0, 50);               // 20% transparent black background
+  // L3.background(0, 50);               // 20% transparent black background
+  // L4.background(0, 50);               // 20% transparent black background
+  // L5.background(0, 50);               // 20% transparent black background
 }
 
+let wheel;
+
 function draw() {
-  circle(100, 100, 40);
-  L1.circle(200, 100, 40);
-  L2.circle(300, 100, 40);
-  L3.circle(400, 100, 40);
-  L4.circle(500, 100, 40);
-  L5.circle(600, 100, 40);
-  image(L1, 0, 0);
-  image(L2, 0, 0);
-  image(L3, 0, 0);
-  image(L4, 0, 0);
-  image(L5, 0, 0);
+  background(0);
+  let wheel = new wheelObject(width / 2, height / 2, 250);
+
+
+  wheel.show();
+  // wheel.animate();
+  // image(L1, 0, 0);
+  // image(L2, 0, 0);
+  // image(L3, 0, 0);
+  // image(L4, 0, 0);
+  // image(L5, 0, 0);
+
+
+  // grid
+  for (let x = 0; x < width; x += 20) {
+    strokeWeight(1);
+    stroke(150, 50);
+    line(x, 0, x, width);
+    line(0, x, width, x);
+  }
 
 }
