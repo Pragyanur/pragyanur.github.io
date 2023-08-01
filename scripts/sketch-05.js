49e5653eb4aa22a728adf70681e89554692d2759
Pragyanur <pragyanur.babu@gmail.com>
2023-08-01 16:43:22 +0530

    update

--- scripts/sketch-05.js
+++ scripts/sketch-05.js
@@ -1,16 +1,29 @@
 const objSize = 10;
+const speed = 0;
 
 class Object {
   constructor() {
     this.position = createVector(random(width), random(height));
-    this.velocity = createVector(3, 3);
+    this.velocity = createVector(speed, speed);
   }
 
+  show() {
+    let centerX = width / 2;
+    let centerY = height / 2;
+    strokeWeight(5);
+    line(centerX, centerY, centerX + this.velocity.x, centerY + this.velocity.y);
+  }
+}
+
+function keyPressed() {
+  if(keyCode === UP_ARROW) {
+
+  }
 }
 
 
-
 function setup() {
+  createrCanvas()
 
 }
 
