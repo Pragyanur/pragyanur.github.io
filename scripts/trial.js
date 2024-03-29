74588e227fc4697977b3c2050628c7eb60f45029
Pragyanur <pragyanur.babu@gmail.com>
2024-03-29 20:48:44 +0530

    update

--- scripts/trial.js
+++ scripts/trial.js
@@ -1,56 +1,54 @@
-const MAX_SPEED = 20;
-let bees = [];
-let count = 0;
+class Colors {
+  constructor() {
+    this.color1 = [239, 188, 155];
+    this.color2 = [251, 243, 213];
+    this.color3 = [214, 218, 200];
+    this.color4 = [156, 175, 170];
+    this.blue1 = [227, 253, 253];
+    this.blue2 = [203, 241, 245];
+    this.blue3 = [166, 227, 233];
+    this.blue4 = [113, 201, 206];
+  }
+  colorList() {
+    return [this.color1, this.color2, this.color3, this.color4, this.blue1, this.blue2, this.blue3, this.blue4];
+  }
+}
+let c = new Colors();
+let circles = [];
 
-class Bee {
-  constructor(x, y) {
-    this.pos = createVector(x, y);
-    this.vel = createVector(2, 2);
-    this.size = 10;
-    this.col = 200;
-  }
-  proximity(bee) {
-    return 1 / (dist(bee.pos.x, bee.pos.y, this.pos.x, this.pos.y) + 0.1);
-  }
-  update() {
-    let flowX = random(-0.5, 0.5);
-    let flowY = random(-0.5, 0.5);
-    this.pos.x = this.pos.x >= width ? 0 : this.pos.x + this.vel.x + flowX;
-    this.pos.y = this.pos.y >= height ? 0 : this.pos.y + this.vel.y + flowY;
-  }
-  behaviour(bees) {
-    for (let bee of bees) {
-      if (dist(this.pos.x, this.pos.y, bee.pos.x, bee.pos.y) < threshhold) {
-        
-      }
-    }
-
+class Circle {
+  constructor(posX, posY, size, angle, speed) {
+    this.position = createVector(posX, posY);
+    this.diameter = size;
+    this.angle = angle;
+    this.speed = speed;
   }
   show() {
-    fill(this.col);
-    circle(this.pos.x, this.pos.y, this.size);
+    noFill();
+    strokeWeight(10);
+    stroke(...c.blue3);
+    arc(this.position.x, this.position.y, this.diameter, this.diameter, 0, this.angle);
   }
+  update() {
+    rotate(frameCount);
+    translate(-this.position.x, -this.position.y);
+  }
+
 }
 
 function setup() {
-  let cnv = createCanvas(windowWidth, windowHeight);
-  // cnv.mouseClicked(createBee);
-
-  for (let j = 0; j < 10; j++) {
-    for (let i = 0; i < 10; i++) {
-      bees[count++] = new Bee(i * 20, j * 20);
-    }
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+  frameRate(60);
+  for (let i = 0; i < 4; i++) {
+    let k = new Circle(width / 2, height / 2, 100, 70, 0);
+    circles.push(k);
   }
 }
-
 function draw() {
-  background(10);
-  for (let bee of bees) {
-    bee.update();
-    bee.show();
+  background(20);
+  for (let o of circles) {
+    o.update();
+    o.show();
   }
 }
-
-// function createBee() {
-//   bees[count++] = new Bee(mouseX, mouseY);
-// }
