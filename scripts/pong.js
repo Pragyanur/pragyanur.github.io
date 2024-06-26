1adf30f59ab9aaadc092f462cf6e4ee7e0730ae3
Pragyanur <pragyanur.babu@gmail.com>
2024-06-27 01:58:13 +0530

    update

--- scripts/pong.js
+++ scripts/pong.js
@@ -1,4 +1,4 @@
-const BALL_SIZE = 10;
+const BALL_SIZE = 20;
 const SPEED = 10;
 let Player1;
 let B1;
@@ -6,33 +6,49 @@ let ball;
 let pfSize = 500;
 let keys = {};
 
+function dotProd(V1, V2) {
+  return V1.x * V2.x + V1.y * V2.y;
+}
+
+function keyPressed() {
+  keys[keyCode] = true;
+}
+
+function keyReleased() {
+  keys[keyCode] = false;
+}
+
+
 class Ball {
   constructor(x = width / 2, y = height / 2) {
     this.pos = createVector(x, y);
     this.vel = createVector(random(-2, 2), 0.1);
     this.fac = SPEED;
   }
+
   update() {
     this.vel.normalize();
     this.pos.x += this.vel.x * this.fac;
     this.pos.y += this.vel.y * this.fac;
     if (this.pos.x < -width / 2 + BALL_SIZE / 2 || this.pos.x > width / 2 - BALL_SIZE / 2) this.vel.x = -this.vel.x;
     if (this.pos.y < -height / 2 + BALL_SIZE / 2 || this.pos.y > height / 2 - BALL_SIZE / 2) this.vel.y = -this.vel.y;
-    if (this.fac != 5) this.fac += this.fac > SPEED ? -1 : 1;
+    if (this.fac != SPEED) this.fac += this.fac > SPEED ? -0.5 : 0.5;
     // debug
     if (this.pos.x < -width / 2) this.pos.x += 5;
     if (this.pos.x > width / 2) this.pos.x -= 5;
     if (this.pos.y < -height / 2) this.pos.y += 5;
     if (this.pos.y > height / 2) this.pos.y -= 5;
   }
-  reflect(player) {
-    if (dist(player.line_1, player.pos_1, this.pos.x, this.pos.y) < player.size - BALL_SIZE / 2) {
-      this.vel = player.hit(this);
+
+  reflect(players) {
+    if (dist(players.line_1, players.pos_1, this.pos.x, this.pos.y) < players.size - BALL_SIZE / 2) {
+      this.vel = players.hit(this);
     }
-    if (dist(player.line_2, player.pos_2, this.pos.x, this.pos.y) < player.size - BALL_SIZE / 2) {
-      this.vel = player.hit(this);
+    if (dist(players.line_2, players.pos_2, this.pos.x, this.pos.y) < players.size - BALL_SIZE / 2) {
+      this.vel = players.hit(this);
     }
   }
+
   show() {
     push();
     translate(this.pos.x, this.pos.y);
@@ -47,7 +63,7 @@ class Ball {
 
 class Players {
   constructor() {
-    this.size = height / 10;
+    this.size = height / 8;
     this.glow_1 = 0;
     this.glow_2 = 0;
     this.rot_1 = 0;
@@ -85,7 +101,7 @@ class Players {
 
   handleKeys() {
     const rotationSpeed = 0.01;
-    const positionSpeed = 10;
+    const positionSpeed = 15;
 
     if (keys[UP_ARROW]) this.pos_1 -= positionSpeed;
     if (keys[DOWN_ARROW]) this.pos_1 += positionSpeed;
@@ -102,7 +118,7 @@ class Players {
     if (this.glow_2 != 100) this.glow_2 *= 0.9;
 
     this.pos_1 = constrain(this.pos_1, -height / 2 + this.size / 2, height / 2 - this.size / 2);
-    this.rAcc_1 = constrain(this.rAcc_1, -1, 1);
+    this.rAcc_1 = constrain(this.rAcc_1, -2, 2);
     this.rot_1 += this.rAcc_1;
     this.rot_1 = this.rot_1 % PI;
 
@@ -127,8 +143,8 @@ class Players {
     let shortestDist_1 = abs((A.y - B.y) * ball.pos.x - (A.x - B.x) * ball.pos.y + (A.x * B.y - A.y * B.x)) / sqrt((A.y - B.y) ** 2 + (A.x - B.x) ** 2);
     let shortestDist_2 = abs((A2.y - B2.y) * ball.pos.x - (A2.x - B2.x) * ball.pos.y + (A2.x * B2.y - A2.y * B2.x)) / sqrt((A2.y - B2.y) ** 2 + (A2.x - B2.x) ** 2);
 
-    line(this.line_1, this.pos_1, this.line_1 + normal_1.x, this.pos_1 + normal_1.y);
-    line(this.line_2, this.pos_2, this.line_2 - normal_2.x, this.pos_2 - normal_2.y);
+    // line(this.line_1, this.pos_1, this.line_1 + normal_1.x, this.pos_1 + normal_1.y);
+    // line(this.line_2, this.pos_2, this.line_2 - normal_2.x, this.pos_2 - normal_2.y);
 
     normal_1.normalize();
     normal_2.normalize();
@@ -138,36 +154,29 @@ class Players {
     let dotProduct_2 = dotProd(initialVelocity, normal_2);
 
     if (shortestDist_1 < BALL_SIZE / 2) {
-      this.glow_1 = 255;
       // r=d−2(d⋅n)n
       let reflect = createVector(initialVelocity.x - 2 * dotProduct_1 * normal_1.x, initialVelocity.y - 2 * dotProduct_1 * normal_1.y);
+      this.glow_1 = 255;
+      ball.fac += abs(dotProduct_1 * this.rAcc_1) * SPEED ** 3;
       return reflect;
     }
     if (shortestDist_2 < BALL_SIZE / 2) {
       let reflect = createVector(initialVelocity.x - 2 * dotProduct_2 * normal_2.x, initialVelocity.y - 2 * dotProduct_2 * normal_2.y);
       this.glow_2 = 255;
+      ball.fac += abs(dotProduct_2 * this.rAcc_2) * SPEED ** 3;
       return reflect;
     }
     return ball.vel;
   }
 }
 
-function dotProd(V1, V2) {
-  return V1.x * V2.x + V1.y * V2.y;
-}
-
-function keyPressed() {
-  keys[keyCode] = true;
-  console.log(keyCode);
-}
-
-function keyReleased() {
-  keys[keyCode] = false;
-}
 
 class Background {
   constructor(platform) {
     this.platformSize = platform;
+    this.leftGlow = 70;
+    this.rightGlow = 70;
+    this.goalPost = 0;
   }
   show() {
     let h = 4 * height / 5;
@@ -196,7 +205,7 @@ class Background {
     plane(width, h / 2);
     pop();
     // left
-    fill(100, 0, 0);
+    fill(this.leftGlow, 0, 0);
     noStroke();
     push();
     translate(-width / 2, -height / 2 + height / 20);
@@ -209,7 +218,7 @@ class Background {
     plane(h / 2, height / 10);
     pop();
     //right
-    fill(0, 0, 100);
+    fill(0, 0, this.rightGlow);
     noStroke();
     push();
     translate(width / 2, -height / 2 + height / 20);
@@ -242,6 +251,27 @@ class Background {
     box(width, height, 0);
     pop();
   }
+
+  reset() {
+    if (this.leftGlow > 70) this.leftGlow -= 10;
+    else this.leftGlow = 70;
+
+    if (this.rightGlow > 70) this.rightGlow -= 10;
+    else this.rightGlow = 70;
+
+  }
+
+  isGoal(ball) {
+    let h = height / 8;
+    if (ball.pos.y < height / 2 - h && ball.pos.y > -height / 2 + h) {
+      if (ball.pos.x <= - width / 2 + BALL_SIZE) {
+        this.leftGlow = 255;
+      }
+      if (ball.pos.x >= width / 2 - BALL_SIZE) {
+        this.rightGlow = 255;
+      }
+    }
+  }
 }
 
 function setup() {
@@ -256,7 +286,7 @@ function draw() {
   background(10);
   pointLight(255, 255, 255, 0, 0, 100);
   // orbitControl();
-  camera(0, 0, 800, ball.pos.x * 0.2, ball.pos.y * 0.2, 0);
+  camera(0, 0, 850, ball.pos.x * 0.2, ball.pos.y * 0.2, 0);
   B1.show();
   ball.show();
   ball.update();
@@ -264,4 +294,6 @@ function draw() {
   Player1.show();
   Player1.handleKeys();
   Player1.update(ball);
+  B1.isGoal(ball);
+  B1.reset();
 }
