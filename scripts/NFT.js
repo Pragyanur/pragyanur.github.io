7ad12a53e2be733d3e405a8e3c4a48a762fc079d
Pragyanur <pragyanur.babu@gmail.com>
2024-07-11 17:53:39 +0530

    update add NFT

--- scripts/NFT.js
+++ scripts/NFT.js
@@ -1,14 +1,27 @@
-count = 0;
+let img1, img2;
+
+function preload() {
+  img1 = createImg("/store/nfts_animation.gif", 'book worm nft gif');
+  img2 = createImg("/store/rare_worms.gif", 'rare worms nfts gif');
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
+  colorMode(HSL, 100);
+
+  img1.style("border-radius: 20px; box-shadow: 5px 5px 0px rgb(20,20,20, 0.5);");
+  img2.style("border-radius: 20px; box-shadow: 5px 5px 0px rgb(20,20,20, 0.5);");
 }
 
-let r = 50; 
-let g = 100; 
-let b = 150; 
-
 function draw() {
-  background(r,g,b);
+  let hue = 100 * abs(sin((frameCount / 500) % 360));
+  background(hue, 100, 95);
+  let padding;
+  padding = width > height ? height / 20 : width / 10;
+  let s;
+  s = height / 2 - padding;
+  img1.size(s, s);
+  img1.position(width / 2 - s/1.7, padding/1.25);
+  img2.size(s, s);
+  img2.position(width / 2 - s/2.5, height / 2 + padding / 5);
 }
